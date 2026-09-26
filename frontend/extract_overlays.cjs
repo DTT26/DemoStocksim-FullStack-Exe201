b85207d543762eead7def2db39bc4dc51d9e8f2b
Trần Đông <dongtvde180115@fpt.edu.vn>
2026-09-26 11:22:48 +0700

    Xóa các file rác (lần cuối)

--- frontend/extract_overlays.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('node_modules/klinecharts/dist/index.esm.js', 'utf8');
-const matches = [...code.matchAll(/name:\s*['"]([a-zA-Z]+)['"]/g)];
-console.log(matches.map(m => m[1]));
